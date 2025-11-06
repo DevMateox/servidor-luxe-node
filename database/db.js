5e9d5bdee0c22391b42be65ec4c94605bd3f1612
DevMateox <nmhx.mate7800@gmail.com>
2025-11-05 20:44:44 -0500

    se agrega el ssl

--- database/db.js
+++ database/db.js
@@ -1,14 +1,15 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-// Configuración de la conexión a PostgreSQL
+// Configuración de conexión correcta para Neon
 export const pool = new Pool({
   host: "ep-royal-term-aem0mw71-pooler.c-2.us-east-2.aws.neon.tech",
   database: "neondb",
   user: "neondb_owner",
   password: "npg_IDVLiNwG7db2",
-  PGSSLMODE: "require",
-  PGCHANNELBINDING: "require",
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
